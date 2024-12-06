5ef84395142710a13fdfb9f8864b53862afb425f
sachin <sk800968@gmail.com>
2024-12-06 11:16:26 +0530

    database connected with .env

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`app is listening at ${process.env.PORT}`);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Testing");
 });
 
-app.listen(PORT, () => {
-  console.log(`app is listening at ${PORT}`);
-});
